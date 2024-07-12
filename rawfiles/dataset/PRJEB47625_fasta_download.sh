4eb1ce17a53a18cdd6e3e480acb054a6a6787c54
Shahram <48684010+bio-informatician@users.noreply.github.com>
2024-07-12 05:09:24 +0200

    Update PRJEB47625_fasta_download.sh

--- rawfiles/dataset/PRJEB47625_fasta_download.sh
+++ rawfiles/dataset/PRJEB47625_fasta_download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -L https://zenodo.org/records/10650983/files/illumina_sample_01_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_01_megahit.fa.gz
-curl -L https://zenodo.org/records/10650983/files/illumina_sample_02_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_02_megahit.fa.gz
-curl -L https://zenodo.org/records/10650983/files/illumina_sample_03_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_03_megahit.fa.gz
-curl -L https://zenodo.org/records/10650983/files/illumina_sample_04_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_04_megahit.fa.gz
+# curl -L https://zenodo.org/records/10650983/files/illumina_sample_02_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_02_megahit.fa.gz
+# curl -L https://zenodo.org/records/10650983/files/illumina_sample_03_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_03_megahit.fa.gz
+# curl -L https://zenodo.org/records/10650983/files/illumina_sample_04_megahit.fa.gz -o workshop_day5/PRJEB47625/illumina_sample_04_megahit.fa.gz
