1c1c9298fbb80ead4eb4a2e548e15dcaa44a7d62
Shahram <48684010+bio-informatician@users.noreply.github.com>
2024-07-12 05:11:25 +0200

    Update PRJEB47625_fastq_download_wget.sh

--- rawfiles/dataset/PRJEB47625_fastq_download_wget.sh
+++ rawfiles/dataset/PRJEB47625_fastq_download_wget.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/006/ERR6797446/ERR6797446_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797446_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/002/ERR6797442/ERR6797442_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797442_1.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/006/ERR6797446/ERR6797446_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797446_1.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/002/ERR6797442/ERR6797442_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797442_1.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/001/ERR6797441/ERR6797441_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797441_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/003/ERR6797443/ERR6797443_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797443_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/004/ERR6797444/ERR6797444_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797444_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/006/ERR6797446/ERR6797446_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797446_2.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/003/ERR6797443/ERR6797443_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797443_2.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/004/ERR6797444/ERR6797444_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797444_2.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/006/ERR6797446/ERR6797446_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797446_2.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/001/ERR6797441/ERR6797441_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797441_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/002/ERR6797442/ERR6797442_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797442_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/005/ERR6797445/ERR6797445_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797445_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/003/ERR6797443/ERR6797443_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797443_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/005/ERR6797445/ERR6797445_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797445_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/004/ERR6797444/ERR6797444_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797444_1.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/002/ERR6797442/ERR6797442_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797442_2.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/005/ERR6797445/ERR6797445_2.fastq.gz -O workshop_day5/PRJEB47625/ERR6797445_2.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/003/ERR6797443/ERR6797443_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797443_1.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/005/ERR6797445/ERR6797445_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797445_1.fastq.gz
+# wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR679/004/ERR6797444/ERR6797444_1.fastq.gz -O workshop_day5/PRJEB47625/ERR6797444_1.fastq.gz
